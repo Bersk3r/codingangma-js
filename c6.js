e40177098932c44df8c9d3670ffaf10c3adf4110
Bersk3r <kiscwook@gmail.com>
2023-06-29 22:19:38 +0900

    c6 clear

---
+++ c6.js
@@ -0,0 +1,56 @@
+// Chapter 6
+// Comparision Operator
+// < > <= >= == !=
+
+// 비교 연산자
+// a = 3 // 대입 연산자
+// a == 3 // a는 3과 같은가?
+// a != 3 // a는 3과 다른가?
+// return = true or false
+// console.log(
+//     (10>5),
+//     (10==5), // 동등 연산자
+//     (10!=5),
+// )
+
+// 동등 연산자
+// const a = 1;
+// const b = "1";
+// console.log(
+//     (a==b),
+//     (a===b), // 일치 연산자
+// )
+
+// 조건문
+// 조건에 따라 어떤 행동을 할 지 결정할 때 사용
+// if문, if-else문, if-elif-else문, switch-case문
+
+// const a = 20;
+// if(a > 19) { // true, false 둘 중 하나 반환
+//     console.log("환영합니다.");
+// }
+// if(a <= 19) { // true, false 둘 중 하나 반환
+//     console.log("안녕히 가세요.");
+// }
+
+// if, else, else if
+// const age = 20;
+// if(age > 19) {
+//     console.log('환영합니다.');
+// } else {
+//     console.log('안녕히 가세요.')
+// }
+// console.log("-----------------");
+
+// const age = 20;
+// if(age > 19) {
+//     console.log('환영합니다.');
+// } else if(age === 19) {
+//     console.log('수능 잘 치세요.');
+// } else {
+//     console.log('안녕히 가세요.')
+// }
+// console.log("-----------------");
+
+
+
