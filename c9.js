e675e66481055207895c7499f6a2905c342e1d2d
Bersk3r <kiscwook@gmail.com>
2023-06-29 23:36:19 +0900

    c9 clear

---
+++ c9.js
@@ -0,0 +1,26 @@
+// Chapter 9
+// Switch Statement
+// switch문은 if-else문보다 케이스가 다양할 때, 간결하게 작성할 수 있다.
+
+// let fruit = "meow";
+//
+// switch(fruit) {
+//     case 'apple':
+//         console.log('100원');
+//         break;
+//     case 'banana':
+//         console.log('200원');
+//         break;
+//     case 'kiwi':
+//         console.log('300원');
+//         break;
+//     case 'melon':
+//     case 'watermelon':
+//         console.log('500원');
+//         break;
+//     default:
+//         console.log('모름');
+// }
+
+
+
